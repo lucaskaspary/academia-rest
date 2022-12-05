c8f0fedddcdb1a01f0788c9befcaf9d590702d9b
lucaskaspary <lucaskaspary@hotmail.com>
2022-12-05 13:43:53 -0300

    Initial commit
    Base project

---
+++ src/main/java/com/lucas/academiarest/AcademiaRestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.academiarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucas/academiarest/AcademiaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.academiarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
